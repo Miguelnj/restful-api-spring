15483a2a574445da7351104754c9d8ac901547a4
Miguel Navarro <miguel.navjor@gmail.com>
2018-05-26 22:12:02 +0200

    file uploads allowed.

--- src/main/java/springtheamproject/project/controller/ImageController.java
+++ src/main/java/springtheamproject/project/controller/ImageController.java
@@ -1,8 +1,15 @@
 package springtheamproject.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import springtheamproject.project.service.ImageService;
+
+import java.io.IOException;
 
 @Controller
 public class ImageController {
@@ -13,4 +20,19 @@ public class ImageController {
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
     }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file){
+        if(file.isEmpty()) return new ResponseEntity<>("Image not selected", HttpStatus.OK);
+
+        try{
+            imageService.save(file);
+        }catch(IOException exception){
+            exception.printStackTrace();
+            return new ResponseEntity<>("I/O error ocurred", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("You successfully uploaded " +
+                file.getOriginalFilename() + "!", HttpStatus.OK);
+    }
+
 }
--- src/main/java/springtheamproject/project/service/ImageService.java
+++ src/main/java/springtheamproject/project/service/ImageService.java
@@ -1,8 +1,19 @@
 package springtheamproject.project.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class ImageService {
 
+    public void save(MultipartFile file) throws IOException {
+        byte[] fileBytes = file.getBytes();
+        Path dataPath = Paths.get("./src/main/resources/images".concat(file.getOriginalFilename()));
+        if(!Files.exists(dataPath)) Files.write(dataPath, fileBytes);
+    }
 }
